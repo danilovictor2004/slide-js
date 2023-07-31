dd63928bb0d8793ca774837310b7e8e2c2bd41f4
danillo.caetano <danillo.caetano@unimedmaceio.com.br>
2023-07-31 11:55:47 -0300

    :sparkles: feat: Move next and preview in navigation

--- js/slide.js
+++ js/slide.js
@@ -5,6 +5,10 @@ export default class Slide {
         this.dist = { finalPosition: 0, startX: 0, movement: 0 };
     }
 
+    transition(active) {
+        this.slide.style.transition = active ? 'transform .3s' : '';
+    }
+
     moveSlide(distX) {
         this.dist.movePosition = distX;
         this.slide.style.transform = `translate3d(${distX}px, 0, 0)`;
@@ -27,17 +31,31 @@ export default class Slide {
         }
         
         this.wrapper.addEventListener(movetype, this.onMove);
+        this.transition(false);
     }
 
     onMove(event) {
-        const finalPosition = this.updatePosition(event.clientX);
+        const pointerPosition = (event.type === 'mousemove') ? event.clientX : event.changedTouches[0].clientX;
+        const finalPosition = this.updatePosition(pointerPosition);
         this.moveSlide(finalPosition);
     }
 
     onEnd(event) {
-        const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
+        const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
         this.wrapper.removeEventListener(moveType, this.onMove);
         this.dist.finalPosition = this.dist.movePosition;      
+        this.transition(true);
+        this.changeSlideOnEnd();
+    }
+
+    changeSlideOnEnd() {
+        if (this.dist.movement > 120 && this.index.next !== undefined) {
+            this.activeNextSlide();
+        } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+            this.activePrevSlide();
+        } else {
+            this.changeSlide(this.index.active);
+        }
     }
 
     addSlideEvents() {
@@ -82,6 +100,17 @@ export default class Slide {
         this.dist.finalPosition = activeSlide.position;
     }
 
+    activePrevSlide() {
+        if (this.index.prev !== undefined) {
+            this.changeSlide(this.index.prev);
+        }
+    }
+
+    activeNextSlide() {
+        if (this.index.next !== undefined) {
+            this.changeSlide(this.index.next);
+        }
+    }
 
     init() {
         if (this.slide && this.wrapper) {
